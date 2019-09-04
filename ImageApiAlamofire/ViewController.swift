d756565cf2481b948bb18e32f295997ecf982821
russell-archer <russell.archer@mac.com>
2019-09-04 15:02:33 +0100

    Initial Commit

---
+++ ImageApiAlamofire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageApiAlamofire
+//
+//  Created by Russell Archer on 04/09/2019.
+//  Copyright © 2019 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
